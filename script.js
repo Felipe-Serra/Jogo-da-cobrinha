b7b3e79ad7d1b1fb971e765fc8e5febacab6cd5b
Felipe Serra <86814247+Felipe-Serra@users.noreply.github.com>
2021-07-13 20:05:42 -0300

    Movimentação finalizada

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ cobra[0] = {
     x: 8*box,
     y: 8*box
 }
-
 let direction = "right";
+
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -21,23 +21,39 @@ function criarCobrinha() {
     }
 }
 
+
+document.addEventListener('keydown', update);
+
+function update(event) {
+    if(event.keyCode == 37 && direction != 'right') direction = 'left';
+    if(event.keyCode == 38 && direction != 'down') direction = 'up';
+    if(event.keyCode == 39 && direction != 'left') direction = 'right';
+    if(event.keyCode == 40 && direction != 'up') direction = 'down';
+}
+
 function iniciarJogo(){
+    if(cobra[0].x > 15*box && direction == 'right') cobra[0].x = 0;
+    if(cobra[0].x < 0 && direction == 'left') cobra[0].x = 16 * box;
+    if(cobra[0].y > 15*box && direction == 'down') cobra[0].y = 0;
+    if(cobra[0].y < 0 && direction == 'up') cobra[0].y = 16 * box;
+
     criarBG();
     criarCobrinha();
+    
 
-    let cobrax = cobra[0].x;
-    let cobray = cobra[0].y;
+    let cobraX = cobra[0].x;
+    let cobraY = cobra[0].y;
 
-    if(direction=="right") cobrax += box;
-    if(direction=="left") cobrax -= box;
-    if(direction=="up") cobray += box;
-    if(direction=="down") cobray -= box;
+    if(direction=="right") cobraX += box;
+    if(direction=="left") cobraX -= box;
+    if(direction=="up") cobraY -= box;
+    if(direction=="down") cobraY += box;
 
     cobra.pop();
 
     let novaCabeça = {
-        x: cobrax,
-        y: cobray
+        x: cobraX,
+        y: cobraY
     }
 
     cobra.unshift(novaCabeça)
